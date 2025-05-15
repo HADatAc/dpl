7a2f8e5083c28ccb68a4317a062537c670f8f39c
Nuno Simões <105175988+numibesi@users.noreply.github.com>
2025-05-15 15:33:14 +0100

    Add instance label

--- src/Form/AddInstanceForm.php
+++ src/Form/AddInstanceForm.php
@@ -198,7 +198,8 @@ class AddInstanceForm extends FormBase {
       $acquisitionDate = $form_state->getValue('instance_acquisition_date');
     }
 
-    $label = Utils::labelFromAutocomplete($form_state->getValue('instance_type')) . " with ID# " . $form_state->getValue('instance_serial_number');
+    // $label = Utils::labelFromAutocomplete($form_state->getValue('instance_type')) . " with ID# " . $form_state->getValue('instance_serial_number');
+    $label = "Instance of [" . Utils::labelFromAutocomplete($form_state->getValue('instance_type')) . "] with Serial Number: [" . $form_state->getValue('instance_serial_number') . "].";
 
     try{
       $useremail = \Drupal::currentUser()->getEmail();

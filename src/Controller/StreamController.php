74cdbf0886674c8a13345e133bae9327b9ec9535
Nuno Simões <105175988+numibesi@users.noreply.github.com>
2025-05-27 02:11:39 +0100

    stream btos, draft

---
+++ src/Controller/StreamController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\dpl\Controller;
+use Drupal\Core\Controller\ControllerBase;
+
+class StreamController extends ControllerBase {
+
+  public function streamRecord($streamUri) {
+    // Your logic to handle the stream record display.
+  }
+
+  public function streamPlay($streamUri) {
+    // Your logic to handle the stream playback.
+  }
+
+  public function streamPause($streamUri) {
+    // Your logic to handle the stream pause.
+  }
+
+  public function streamStop($streamUri) {
+    // Your logic to handle stopping the stream.
+  }
+
+}

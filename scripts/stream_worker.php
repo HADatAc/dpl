16d2f000934f1a6de02c6a04de718152fc9a1026
Tiago Moreira <tiago.moreira@ipiaget.pt>
2025-06-04 15:44:25 +0100

    updates

--- scripts/stream_worker.php
+++ scripts/stream_worker.php
@@ -3,9 +3,17 @@
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
-define('DRUPAL_ROOT', dirname(dirname(dirname(dirname(__DIR__)))));
-require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
-drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+use Symfony\Component\HttpFoundation\Request;
+use Drupal\Core\DrupalKernel;
+
+$autoloader = require_once '/opt/drupal/web/autoload.php';
+
+$kernel = DrupalKernel::createFromRequest(
+  Request::createFromGlobals(),
+  $autoloader,
+  'dev'
+);
+$kernel->boot();
 
 $stream_id = $argv[1];
 $archive_id = $argv[2];
@@ -15,20 +23,19 @@ $topic = $argv[5];
 
 $fs = \Drupal::service('file_system');
 $directory = 'private://streams/messageFiles/';
-$fs->prepareDirectory($directory, \Drupal\Core\File\FileSystemInterface::CREATE_DIRECTORY | FileSystemInterface::MODIFY_PERMISSIONS);
+$fs->prepareDirectory($directory, \Drupal\Core\File\FileSystemInterface::CREATE_DIRECTORY | \Drupal\Core\File\FileSystemInterface::MODIFY_PERMISSIONS);
 
 $filepath = $fs->realpath($directory . "Messages{$archive_id}_0.xlsx");
 $last_message = '';
 
 while (true) {
-  // Check stream status
   $api = \Drupal::service('rep.api_connector');
   $stream = $api->parseObjectResponse($api->getUri($stream_id), 'getUri');
+
   if ($stream->hasMessageStatus !== 'http://hadatac.org/ont/hasco/RECORDING') {
     break;
   }
 
-  // Read last MQTT message via tmux
   $ssh_cmd = "ssh -i /var/www/.ssh/graxiom_main.pem -o StrictHostKeyChecking=no ubuntu@$ip 'tmux capture-pane -pt " . escapeshellarg($topic) . " -S -1 -e'";
   $output = shell_exec($ssh_cmd);
 
